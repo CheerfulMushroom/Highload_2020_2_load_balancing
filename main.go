33963abd21a439f14a33e9eb11b4aaf59844c3f6
Lebedev Alexandr <s.lebedev.2011@bk.ru>
2020-10-13 11:23:15 +0300

    Added hits counter

--- main.go
+++ main.go
@@ -2,20 +2,45 @@ package main
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"log"
 	"math/rand"
 	"net/http"
 	"time"
 )
 
+var hitsCounter = promauto.NewCounter(prometheus.CounterOpts{
+	Name: "hits_counter",
+	Help: "The total number of hits",
+})
+
+func ServeContent(ctx echo.Context) error {
+	const maxWaitingTime = 1000
+	time.Sleep(time.Duration(rand.Intn(maxWaitingTime)) * time.Millisecond)
+	return ctx.String(http.StatusOK, "Hello, I am fast backend server\n")
+}
+
+
+func IncreaseHits(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		hitsCounter.Inc()
+		return next(c)
+	}
+}
+
 func main() {
+
+	go func() {
+		prometheusRouter := echo.New()
+		prometheusRouter.GET("/metrics", echo.WrapHandler(promhttp.Handler()))
+		log.Fatal(prometheusRouter.Start(":5050"))
+	}()
+
 	router := echo.New()
-	router.GET("*",
-		func(ctx echo.Context) error {
-			const maxWaitingTime = 1000
-			time.Sleep(time.Duration(rand.Intn(maxWaitingTime)) * time.Millisecond)
-			return ctx.String(http.StatusOK, "Hello, I am fast backend server\n")
-		},
+	router.Use(IncreaseHits)
+	router.GET("/", ServeContent,
 	)
 	log.Fatal(router.Start(":8080"))
 }
