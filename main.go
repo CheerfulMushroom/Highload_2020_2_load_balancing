7f656dca09214b5792fe12442d93447fb9a1cd6f
Lebedev Alexandr <s.lebedev.2011@bk.ru>
2020-10-13 10:56:51 +0300

    Added delay

--- main.go
+++ main.go
@@ -3,13 +3,17 @@ package main
 import (
 	"github.com/labstack/echo/v4"
 	"log"
+	"math/rand"
 	"net/http"
+	"time"
 )
 
 func main() {
 	router := echo.New()
 	router.GET("*",
 		func(ctx echo.Context) error {
+			const maxWaitingTime = 1000
+			time.Sleep(time.Duration(rand.Intn(maxWaitingTime)) * time.Millisecond)
 			return ctx.String(http.StatusOK, "Hello, I am fast backend server\n")
 		},
 	)
